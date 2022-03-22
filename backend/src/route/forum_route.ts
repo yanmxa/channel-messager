48f7b26843a44dca211df210f5c13dd703516952
Meng <yanmxa@163.com>
2022-03-22 15:32:46 +0800

    add await for rest service

--- backend/src/route/forum_route.ts
+++ backend/src/route/forum_route.ts
@@ -4,10 +4,10 @@ import * as bodyParser from "body-parser";
 
 export const RestRouter = express.Router()
 RestRouter.use(bodyParser.json())
-RestRouter.post("/channel", async (req: Request, res: Response) => restService.addChannel(req, res));
-RestRouter.get("/channels", async (req: Request, res: Response) => restService.listChannel(req, res));
-RestRouter.post("/message", async (req: Request, res: Response) => restService.addMessage(req, res));
-RestRouter.get("/messages", async (req: Request, res: Response) => restService.listMessage(req, res));
+RestRouter.post("/channel", async (req: Request, res: Response) => await restService.addChannel(req, res));
+RestRouter.get("/channels", async (req: Request, res: Response) => await restService.listChannel(req, res));
+RestRouter.post("/message", async (req: Request, res: Response) => await restService.addMessage(req, res));
+RestRouter.get("/messages", async (req: Request, res: Response) => await restService.listMessage(req, res));
 
 
 export const GraphglRouter = express.Router()
